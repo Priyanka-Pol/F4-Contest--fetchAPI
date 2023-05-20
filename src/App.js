83d53a22711bdbc83d952b7f5f913f271e5781ca
Priyanka-Pol <priyankapol2019@gmail.com>
2023-05-20 23:22:42 +0530

    fetch API

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import TableRow from './App.test';
+
+const ApiApp = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+        );
+        setData(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Top 10 Cryptocurrencies in the world</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>ID</th>
+            <th>Image</th>
+            <th>Symbol</th>
+            <th>Price</th>
+            <th>Volume</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => (
+            <TableRow key={item.id} data={item} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ApiApp;
